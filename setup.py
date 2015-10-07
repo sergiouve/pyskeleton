8543b37c32ceedf66db9760ab1fe2f04a344ec1b
Sergio Uve <sergiouve@gmail.com>
2015-10-08 00:11:18 +0200

    Skeleton ready to go

--- setup.py
+++ setup.py
@@ -1,2 +1,19 @@
 try:
-    
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'New project',
+    'author': 'Sergio Uve',
+    'url': 'http://new-project.com',
+    'download_url': 'hhtp://new-project.com/download',
+    'author_email': 'sergiouve@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'project_name'
+}
+
+setup(**config)
